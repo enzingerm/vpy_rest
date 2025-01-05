ab353cfcef5c6a4324708cababb449c470144e5c
Marinus Enzinger <marinus@enzingerm.de>
2025-01-05 08:17:19 +0100

    Update dependencies and start sanic as proposed in the docs

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import asyncio
+import uvloop
 
 from sanic import Sanic
 
@@ -24,18 +25,19 @@ def create_api(conn: ConnectionCache, api_cfg):
     return Api(conn, auth_provider, api_parts)
 
 
-def main():
+async def main():
     loop = asyncio.get_event_loop()
     cfg = get_config(loop)
     cfg.device.conn.start_communication()
     api = create_api(cfg.device, cfg.api)
     app.blueprint(api.get_blueprint())
-    server = app.create_server(
+    server = await app.create_server(
         host=cfg.server.ip, port=cfg.server.port, return_asyncio_server=True
     )
-    loop.create_task(server)
-    loop.run_forever()
+    await server.startup()
+    await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.set_event_loop(uvloop.new_event_loop())
+    asyncio.run(main())
